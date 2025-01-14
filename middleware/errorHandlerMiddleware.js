fd6035fa4c7ec98ea93081c8d3db693bd3dc4b95
Reza Zahedi <r.zahedi@gmail.com>
2025-01-14 15:37:20 -0800

    Remove unused console

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -5,10 +5,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-
-  // TODO: Log the error in development mode!
-  console.log(err)
-  
   return res.status( StatusCodes.INTERNAL_SERVER_ERROR ).json({ err })
 }
 
