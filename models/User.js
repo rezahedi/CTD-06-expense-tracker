39ed06cf21ce21dc1b7dcda5a5a691c48ce1d6d9
Reza Zahedi <r.zahedi@gmail.com>
2025-01-14 21:30:14 -0800

    Salt the pass by mongoose middleware

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,13 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Password is required'],
     minLength: [6, 'Password is too short'],
   },
+})
+
+// Salt the password by mongoose middleware
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 module.exports = mongoose.model('User', UserSchema)
