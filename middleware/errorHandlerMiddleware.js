4158cd1f1f93b1885bbcc5a9b713c1c6f8f7b9ed
Reza Zahedi <r.zahedi@gmail.com>
2025-01-22 16:41:21 -0800

    Handle Mongoose error starting with duplicate error

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -2,10 +2,23 @@ const { CustomError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // Set default or set by custom error's values
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong.'
   }
-  return res.status( StatusCodes.INTERNAL_SERVER_ERROR ).json({ err })
+
+  // Check if error comes from Mongoose and which error code is
+  // 11000 = Duplicate value for unique field
+  if ( err.code && err.code === 11000 ) {
+    customError = {
+      msg: `Duplicate value entered for unique '${Object.keys(err.keyValue)[0]}' field, please use different value.`,
+      statusCode: StatusCodes.BAD_REQUEST,
+    }
+  }
+  
+  // return res.status( StatusCodes.INTERNAL_SERVER_ERROR ).json({ err })
+  return res.status( customError.statusCode ).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
