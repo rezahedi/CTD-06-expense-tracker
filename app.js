0a2bb36ced1519eb7d8a0be2397d65f6d65a188a
Reza Zahedi <r.zahedi@gmail.com>
2025-01-22 23:16:19 -0800

    Add api docs using swagger & yaml

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 app.set('trust proxy', 1)
 app.use(rateLimiter({
   windowMs: 60_000, // 6 min
@@ -27,6 +32,11 @@ app.use(cors())
 app.use(xss())
 
 // routes
+app.get('/', (req, res) => {
+  res.send('<h1>Expense API</h1><p><a href="/api-docs">The API Documentation</a></p>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/expenses', authMiddleware, expensesRouter)
 
